6f6dfb09401f54529b60cb4ff0fd71ab984eddd8
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-04-19 11:00:16 +0900

    Refactor password page to use withAuthenticated higher-order component (HOC)

--- src/app/settings/password/page.tsx
+++ src/app/settings/password/page.tsx
@@ -1,26 +1,6 @@
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { redirect } from "next/navigation";
-import { Database } from "../../../../lib/database.types";
 import Password from "@/app/components/password";
+import withAuthenticated from "../../../../HOC";
 
 // パスワード変更ページ
-const PasswordPage = async () => {
-  const supabase = createServerComponentClient<Database>({
-    cookies,
-  });
-
-  // セッションの取得
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  // 未認証の場合、リダイレクト
-  if (!session) {
-    redirect("/auth/login");
-  }
-
-  return <Password />;
-};
-
-export default PasswordPage;
+const PasswordPage = () => <Password />;
+export default withAuthenticated(PasswordPage);
