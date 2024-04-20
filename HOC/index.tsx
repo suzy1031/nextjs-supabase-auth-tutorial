24bdfb488cb8df6414191371aac494a8dab82e0f
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-04-21 08:08:37 +0900

    Refactor Supabase authentication code in index.tsx to use serverSide function

--- HOC/index.tsx
+++ HOC/index.tsx
@@ -1,16 +1,11 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { Database } from "../lib/database.types";
 import { redirect } from "next/navigation";
 import React from "react";
+import { serverSide } from "../lib/api/client";
 
 const withAuthenticated = (WrappedComponent: () => JSX.Element) => {
   const Auth = async (props: JSX.IntrinsicAttributes) => {
-    const supabase = createServerComponentClient<Database>({ cookies });
-
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
+    const session = (await serverSide(cookies)).getSession();
 
     if (!session) {
       redirect("/auth/login");
