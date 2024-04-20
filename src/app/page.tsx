dec36faabdc05a51e33d1fbfff0d0223d7f1556f
suzy1031 <kohei_sugawara@hotmail.co.jp>
2024-04-21 08:08:42 +0900

    Refactor Supabase authentication code in page.tsx to use getSession function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "../../lib/database.types";
-import { cookies } from "next/headers";
+import { getSession } from "../../lib/api/server";
 
 const Home = async () => {
-  const supabase = createServerComponentClient<Database>({
-    cookies,
-  });
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+  const session = await getSession();
 
   return (
     <div className="text-center text-xl">
